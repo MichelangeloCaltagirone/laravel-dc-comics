e148415672651a9ad0764037f129b8a6876cf04d
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-30 16:15:57 +0100

    Prepared the db's table via migration, and populated it by seeder.

--- database/migrations/2024_10_29_163018_create_comics_table.php
+++ database/migrations/2024_10_29_163018_create_comics_table.php
@@ -6,13 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->text("description");
+            $table->text("image_url");
+            $table->float("price");
+            $table->string("series");
+            $table->date("sale_date");
+            $table->string("type");
             $table->timestamps();
         });
     }
