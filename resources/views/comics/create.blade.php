3d1926bf1cf895c34c940ed90e614b769a0a4d81
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-31 16:12:26 +0100

    Add better style for the create comic form.

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,45 +6,48 @@
 
 <div class="container">
 
-    <form class="card" action="{{ route("comics.store") }}" method="POST">
+    <form class="card col-8 px-3 m-auto" action="{{ route("comics.store") }}" method="POST">
     @csrf
         <div class="my-1">
-            <label for="comic-name" class="form-label">Name:</label>
+            <label for="comic-name" class="form-label ps-2">Name:</label>
             <input type="text" class="form-control" id="comic-name" name="name">
         </div>
 
         <div class="my-1">
-            <label for="comic-description" class="form-label">Description:</label>
+            <label for="comic-description" class="form-label ps-2">Description:</label>
             <input type="text" class="form-control" id="comic-description" name="description">
         </div>
 
         <div class="my-1">
-            <label for="comic-image" class="form-label">Image Url:</label>
+            <label for="comic-image" class="form-label ps-2">Image Url:</label>
             <input type="text" class="form-control" id="comic-image" name="image_url">
         </div>
 
         <div class="my-1">
-            <label for="comic-price" class="form-label">Price:</label>
+            <label for="comic-price" class="form-label ps-2">Price:</label>
             <input type="text" class="form-control" id="comic-price" name="price">
         </div>
 
         <div class="my-1">
-            <label for="comic-series" class="form-label">Series:</label>
+            <label for="comic-series" class="form-label ps-2">Series:</label>
             <input type="text" class="form-control" id="comic-series" name="series">
         </div>
 
         <div class="my-1">
-            <label for="comic-sale_date" class="form-label">Sale date:</label>
+            <label for="comic-sale_date" class="form-label ps-2">Sale date:</label>
             <input type="text" class="form-control" id="comic-sale_date" name="sale_date">
         </div>
 
         <div class="my-1">
-            <label for="comic-type" class="form-label">Type:</label>
+            <label for="comic-type" class="form-label ps-2">Type:</label>
             <input type="text" class="form-control" id="comic-type" name="type">
         </div>
 
-        <button class="btn btn-lg btn-success" type="submit">Invia</button>
-        <button class="btn btn-lg btn-warning" type="reset">Svuota campi</button>
+        <div class="my-1 d-flex justify-content-around">
+            <button class="btn btn-success my-2 px-3" type="submit">Invia</button>
+            <button class="btn btn-warning my-2 px-3" type="reset">Svuota campi</button>
+        </div>
+
 
     </form>
 
