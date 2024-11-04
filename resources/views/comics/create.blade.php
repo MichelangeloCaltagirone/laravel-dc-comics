21178a0f4158d58eb8e8585522f8c3253295c5d1
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-04 18:30:44 +0100

    Moved the errors' messages under each proper inputs field, in the edit and create form page.

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,7 +6,7 @@
 
 <div class="container">
 
-    @if ($errors->any())
+    {{-- @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
                 @foreach ($errors->all() as $error)
@@ -14,43 +14,78 @@
                 @endforeach
             </ul>
         </div>
-    @endif
+    @endif --}}
 
     <form class="card col-8 px-3 m-auto" action="{{ route("comics.store") }}" method="POST">
     @csrf
         <div class="my-1">
             <label for="comic-name" class="form-label ps-2">Name:</label>
             <input type="text" class="form-control" id="comic-name" name="name" value="{{ old('name') }}">
+            @error("name")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-description" class="form-label ps-2">Description:</label>
             <input type="text" class="form-control" id="comic-description" name="description" value="{{ old('description') }}">
+            @error("description")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-image" class="form-label ps-2">Image Url:</label>
             <input type="text" class="form-control" id="comic-image" name="image_url" value="{{ old('image_url') }}">
+            @error("image_url")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-price" class="form-label ps-2">Price:</label>
             <input type="text" class="form-control" id="comic-price" name="price" value="{{ old('price') }}">
+            @error("price")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-series" class="form-label ps-2">Series:</label>
             <input type="text" class="form-control" id="comic-series" name="series" value="{{ old('series') }}">
+            @error("series")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-sale_date" class="form-label ps-2">Sale date:</label>
             <input type="text" class="form-control" id="comic-sale_date" name="sale_date" value="{{ old('sale_date') }}">
+            @error("sale_date")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-type" class="form-label ps-2">Type:</label>
             <input type="text" class="form-control" id="comic-type" name="type" value="{{ old('type') }}">
+            @error("type")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1 d-flex justify-content-around">
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,7 +5,8 @@
 @section("main-content")
 
 <div class="container">
-    @if ($errors->any())
+
+    {{-- @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
                 @foreach ($errors->all() as $error)
@@ -13,7 +14,7 @@
                 @endforeach
             </ul>
         </div>
-    @endif
+    @endif --}}
 
     <form class="card col-8 px-3 m-auto" action="{{ route("comics.update", $comic->id) }}" method="POST">
         @method("PUT")
@@ -22,42 +23,77 @@
         <div class="my-1">
             <label for="comic-name" class="form-label ps-2">Name:</label>
             <input type="text" class="form-control" id="comic-name" name="name" value="{{ old('name', $comic->name) }}">
+            @error("name")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-description" class="form-label ps-2">Description:</label>
             <input type="text" class="form-control" id="comic-description" name="description"
             value="{{ old('description', $comic->description) }}">
+            @error("description")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-image" class="form-label ps-2">Image Url:</label>
             <input type="text" class="form-control" id="comic-image" name="image_url"
             value="{{ old('image_url', $comic->image_url) }}">
+            @error("image_url")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-price" class="form-label ps-2">Price:</label>
             <input type="text" class="form-control" id="comic-price" name="price"
             value="{{ old('price', $comic->price) }}">
+            @error("price")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-series" class="form-label ps-2">Series:</label>
             <input type="text" class="form-control" id="comic-series" name="series"
             value="{{ old('series', $comic->series) }}">
+            @error("series")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-sale_date" class="form-label ps-2">Sale date:</label>
             <input type="text" class="form-control" id="comic-sale_date" name="sale_date"
             value="{{ old('sale_date', $comic->sale_date) }}">
+            @error("sale_date")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1">
             <label for="comic-type" class="form-label ps-2">Type:</label>
             <input type="text" class="form-control" id="comic-type" name="type"
             value="{{ old('type', $comic->type) }}">
+            @error("type")
+                <div class="alert alert-warning">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
 
         <div class="my-1 d-flex justify-content-around">
